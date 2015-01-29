9ec799a88a3c977ed1e04ac3c8b1ba8a6affd50e
mehw <mehw.is.me@inventati.org>
2015-01-29 06:16:54 +0100

    add regexp utility for testing purposes

---
+++ regexp-testing-ground.el
@@ -0,0 +1,68 @@
+;;; regexp-testing-ground.el
+
+;; DEFINE HERE THE PREFIXES
+(defvar test-prefixes
+  '("foo"
+    "bar"))
+
+;; DEFINE HERE THE REGEXPS
+(defconst test-regexps
+  '("^COMPLETION: \\_<\\(%s[a-zA-Z0-9_:]*\\)\\(?: : \\(.+?\\)?\\)?\\(?: : \\(.+?\\)?\\)?$"
+    "^COMPLETION: \\_<\\(%s[a-zA-Z0-9_:]*\\)\\(?: : \\(.+?\\)?\\)?\\(?: : \\(.+\\)?\\)?$"
+    "^COMPLETION: \\_<\\(%s[a-zA-Z0-9_:]*\\)\\(?: : \\(.+?\\)?\\)?\\(?: : \\(.*\\)?\\)?$"))
+
+;; DEFINE HERE THE TARGET STRINGS
+(defvar test-strings
+  '(
+   ;; expected result: no capture (no prefix)
+   "COMPLETION: "
+   ;; expected result: no capture (wrong prefix)
+   "COMPLETION: blabla"
+   ;; expected result: group 1
+   "COMPLETION: foobar"
+   ;; expected result: group 1 (group 2, 3 are nil)
+   "COMPLETION: foobar : "
+   ;; expected result: group 1, 2 (group 3 is nil)
+   "COMPLETION: foobar : [#int#]foobar(<#int a#>)"
+   ;; expected result: group 1, 2 (group 3 is nil)
+   "COMPLETION: foobar : [#int#]foobar(<#int a#>) : "
+   ;; expected result: group 1, 2, 3
+   "COMPLETION: foobar : [#int#]foobar(<#int a#>) : Comment 1"
+   ;; expected result: group 1, 2, 3 ("Comment 1 : ")
+   "COMPLETION: foobar : [#int#]foobar(<#int a#>) : Comment 1 : "))
+
+(defun regexp-testing-ground-run-tests nil
+  (interactive)
+  (let ((results-list) (pattern) (match) (hit))
+    (with-current-buffer (get-buffer-create "*regexp-testing-ground*")
+      (dolist (t-prefix test-prefixes)
+        (dolist (t-regexp test-regexps)
+          (push (concat "prefix : \'" t-prefix "\'") results-list)
+          (push (concat "regexp : \'" t-regexp "\'") results-list)
+          (setq pattern (format t-regexp t-prefix))
+          (push (concat " pattern: \'" pattern "\'") results-list)
+          (dolist (t-string test-strings)
+            (erase-buffer)
+            (insert t-string)
+            (goto-char (point-min))
+            (push (concat "  string : \'" t-string "\'") results-list)
+            (setq hit (re-search-forward pattern nil t))
+            (dolist (group '(1 2 3))
+              (if hit
+                  (setq match (match-string-no-properties group))
+                (setq match nil))
+              (unless match
+                (setq match ""))
+              (push (concat
+                     "   group " (number-to-string group) ": \'"
+                     match "\'")
+                    results-list))))))
+    (setq results-list (reverse results-list))
+    (with-current-buffer (get-buffer-create "*regexp-testing-ground*")
+      (erase-buffer)
+      (dolist (line results-list)
+        (insert line)
+        (insert "\n")))))
+
+(provide 'regexp-testing-ground)
+;;; regexp-testing-ground.el ends here
